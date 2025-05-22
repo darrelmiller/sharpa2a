0de97f1eb65a956bcd095ef1ec24327f242a6b56
Darrel Miller <darrel.miller@microsoft.com>
2025-05-21 23:35:16 -0400

    Fixed SK Travel Agent demo

--- samples/SemanticKernelAgent/Program.cs
+++ samples/SemanticKernelAgent/Program.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using SemanticKernelAgent;
 using SharpA2A.AspNetCore;
 using SharpA2A.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient().AddLogging();
+builder.Services.AddHttpClient()
+    .AddLogging()
+    .AddOpenTelemetry()
+    .ConfigureResource(resource =>
+    {
+        resource.AddService("TravelAgent");
+    })
+    .WithTracing(tracing => tracing
+        .AddSource(SharpA2A.Core.TaskManager.ActivitySource.Name)
+        .AddSource(A2AJsonRpcProcessor.ActivitySource.Name)
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://localhost:4317");
+            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        })
+        );
 var app = builder.Build();
 
 var configuration = app.Configuration;
